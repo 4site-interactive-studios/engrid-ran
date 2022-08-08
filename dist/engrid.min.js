e9d82e8a54a4c558f2dfa387bc9637a2c54b9894
Fernando Santos <fern@ndo.io>
2022-08-08 18:31:23 -0300

    Plaid Tweaks

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Monday, August 8, 2022 @ 17:05:07 ET
+ *  Date: Monday, August 8, 2022 @ 17:30:42 ET
  *  By: fernando
  *  ENGrid styles: v0.13.13
  *  ENGrid scripts: v0.13.13
