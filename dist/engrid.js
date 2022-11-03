e7aa1287674710bb0e4a34784cbb7fd04dc3de3a
bryancasler <bryan.casler@gmail.com>
2022-11-03 17:21:31 -0500

    Match the multistep layout to what you see in Page Builder and when viewing the page directly

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, November 3, 2022 @ 18:08:54 ET
+ *  Date: Thursday, November 3, 2022 @ 18:20:28 ET
  *  By: bryancasler
  *  ENGrid styles: v0.13.13
  *  ENGrid scripts: v0.13.15
