e6cf7f814f8b2d91e41f48b0fb139b5baa6e99fc
Michael Thomas <michaelthomas372@gmail.com>
2023-04-21 15:40:17 +0200

    recompile using latest engrid-scripts from local

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import {
-  Options,
-  App,
-  DonationAmount,
-  DonationFrequency,
-} from "@4site/engrid-common"; // Uses ENGrid via NPM
-// import { Options, App } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
+// import {
+//   Options,
+//   App,
+//   DonationAmount,
+//   DonationFrequency,
+// } from "@4site/engrid-common"; // Uses ENGrid via NPM
+import { Options, App, DonationAmount, DonationFrequency } from "../../engrid-scripts/packages/common"; // Uses ENGrid via Visual Studio Workspace
 import "./sass/main.scss";
 import DonationLightboxForm from "./scripts/donation-lightbox-form";
 import { customScript } from "./scripts/main";
