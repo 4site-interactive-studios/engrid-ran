54c6649554bce8a939a19a13e989bdf5f319bb0d
Fernando Santos <fern@ndo.io>
2024-10-23 17:01:58 -0400

    Adds space for the welcome back message if there's no content there to natively offer it

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Tuesday, October 15, 2024 @ 14:47:20 ET
+ *  Date: Wednesday, October 23, 2024 @ 16:24:36 ET
  *  By: fernando
  *  ENGrid styles: v0.19.9
  *  ENGrid scripts: v0.19.9
