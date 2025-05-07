76b0469881a823be3aaf44b3a3bf89c51f05b57c
Daryl Brewer <brewguy@gmail.com>
2025-05-07 15:06:56 -0400

    adjusted font fix to be desktop only

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Wednesday, May 7, 2025 @ 14:56:14 ET
+ *  Date: Wednesday, May 7, 2025 @ 15:06:29 ET
  *  By: daryl
  *  ENGrid styles: v0.21.0
  *  ENGrid scripts: v0.21.0
