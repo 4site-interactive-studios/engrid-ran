808842462085474c2d94958501da2ea89255557b
Daryl Brewer <brewguy@gmail.com>
2025-05-08 10:22:04 -0400

    Connect desktop font size fix, removed unnecessary !important declarations

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, May 8, 2025 @ 10:12:34 ET
+ *  Date: Thursday, May 8, 2025 @ 10:21:42 ET
  *  By: daryl
  *  ENGrid styles: v0.21.0
  *  ENGrid scripts: v0.21.0
