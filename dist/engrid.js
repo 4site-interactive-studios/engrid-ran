8854234448b2a9101466e9162faef2d148863bfc
Daryl Brewer <brewguy@gmail.com>
2025-05-08 13:04:51 -0400

    QA adjustments

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, May 8, 2025 @ 10:21:42 ET
+ *  Date: Thursday, May 8, 2025 @ 13:02:17 ET
  *  By: daryl
  *  ENGrid styles: v0.21.0
  *  ENGrid scripts: v0.21.0
