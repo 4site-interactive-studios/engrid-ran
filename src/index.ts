2ce156b2e6ac42200cdf40b6968e053e730500e9
npgiano <nick@stone.money>
2026-01-28 14:43:01 -0500

    Test branch for EN tidycontact page_types config.

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,7 @@ const options: Options = {
   // NeverBounceDateFormat: "YYYYMMDD",
   TidyContact: {
     cid: "659b7129-73d0-4601-af4c-8942c4730f65",
+    page_types: ["DONATION"],
     us_zip_divider: "-",
     record_field: "supporter.NOT_TAGGED_41",
     date_field: "supporter.NOT_TAGGED_39",
