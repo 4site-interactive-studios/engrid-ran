f404a1bd2cec24f9fa4db758045052b78e79f5b3
npgiano <nick@stone.money>
2026-06-29 10:08:38 -0400

    Move body banner img to page backgroundImage when no page backgroundImage is set

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,27 @@ export const customScript = function (App, EnForm) {
   App.log("ENGrid client scripts are executing");
   // Add your client scripts here
 
+  // If body-banner has a image, copy it to the body background image
+  const bodyBanner = document.querySelector(".body-banner");
+  const pageBackground = document.querySelector(".page-backgroundImage");
+  if (bodyBanner && pageBackground) {
+    const bodyBannerImage = bodyBanner.querySelector("img");
+    const pageBackgroundImage = pageBackground.querySelector(
+      "img, video, picture"
+    );
+    if (bodyBannerImage && !pageBackgroundImage) {
+      const imageUrl = bodyBannerImage.src;
+      const cssUrl = `url('${imageUrl}')`;
+      pageBackground.style.setProperty(
+        "--engrid__page-backgroundImage_url",
+        cssUrl
+      );
+      App.setBodyData("page-background", "image");
+      document.body.removeAttribute("data-engrid-no-page-backgroundImage");
+      App.log(`Set page background image to: ${imageUrl}`);
+    }
+  }
+
   // If we're on the last page OR we're redirected from another EN Page
   if (
     App.getPageType() !== "UNKNOWN" &&
@@ -77,7 +98,7 @@ export const customScript = function (App, EnForm) {
 
   const substackIframe = document.querySelector('iframe[name="substack"]');
 
-  if(substackIframe) {
+  if (substackIframe) {
     substackIframe.removeAttribute("sandbox");
     substackIframe.src = substackIframe.src;
   }
