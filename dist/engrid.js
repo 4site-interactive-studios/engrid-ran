8fc550d775001c26e5e66349f7fbb4f92936d7d5
plpmd <pedroluanpd@gmail.com>
2026-07-06 15:32:37 -0300

    feat: add mobile display

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Wednesday, July 1, 2026 @ 15:25:50 ET
+ *  Date: Monday, July 6, 2026 @ 14:32:21 ET
  *  By: pedroluan
  *  ENGrid styles: v0.25.6
  *  ENGrid scripts: v0.25.6
